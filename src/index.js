0e511aa7a678e695585cd3a0504a19378f160f50
eshell <eshellster@gmail.com>
2020-02-25 18:14:36 +0900

    프로젝트 시작

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import "./typography";
+import App from "./Component/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const GlobalStyle = createGlobalStyle`
+${reset};
+body{
+    background-color:#ecf0f1;
+}`;
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById("root")
+);
